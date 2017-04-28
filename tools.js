b2f033e8dfd94d5d69f711de9d462d1ea3120c6f
jsoncode <jsoncode@users.noreply.github.com>
2017-04-28 12:05:44 +0800

    checkFileType('(png:jpe)',file,back)

--- tools.js
+++ tools.js
@@ -686,3 +686,83 @@ function uploadFile(file,fileName,back) {
     xhr.open('POST', 'url');
     xhr.send(formdata);
 }
+function checkFileType(type, file, back) {
+    /**
+     * type png jpg mp4 ...
+     * file input.change=> this.files[0]
+     * back callback(boolean)
+     */
+    // http://www.garykessler.net/library/file_sigs.html
+    var args = arguments;
+    if (args.length != 3) {
+        back(0);
+    }
+    var type = args[0]; // type = '(png|jpg)' , 'png'
+    var file = args[1];
+    var back = typeof args[2] == 'function' ? args[2] : function() {};
+    if (file.type == '') {
+        // 如果系统无法获取文件类型，则读取二进制流，对二进制进行解析文件类型
+        var imgType = [
+            'ff d8 ff', //jpg
+            '89 50 4e', //png
+
+            '0 0 0 14 66 74 79 70 69 73 6F 6D', //mp4
+            '0 0 0 18 66 74 79 70 33 67 70 35', //mp4
+            '0 0 0 0 66 74 79 70 33 67 70 35', //mp4
+            '0 0 0 0 66 74 79 70 4D 53 4E 56', //mp4
+            '0 0 0 0 66 74 79 70 69 73 6F 6D', //mp4
+
+            '0 0 0 18 66 74 79 70 6D 70 34 32', //m4v
+            '0 0 0 0 66 74 79 70 6D 70 34 32', //m4v
+
+            '0 0 0 14 66 74 79 70 71 74 20 20', //mov
+            '0 0 0 0 66 74 79 70 71 74 20 20', //mov
+            '0 0 0 0 6D 6F 6F 76', //mov
+
+            '4F 67 67 53 0 02', //ogg
+            '1A 45 DF A3', //ogg
+        ];
+        var typeName = [
+            'jpg',
+            'png',
+            'mp4',
+            'mp4',
+            'mp4',
+            'mp4',
+            'mp4',
+            'm4v',
+            'm4v',
+            'mov',
+            'mov',
+            'mov',
+            'ogg',
+            'ogg',
+        ];
+        var sliceSize = /png|jpg|jpeg/.test(type) ? 3 : 12;
+        var reader = new FileReader();
+        reader.readAsArrayBuffer(file);
+        reader.addEventListener("load", function(e) {
+            var slice = e.target.result.slice(0, sliceSize);
+            reader = null;
+            if (slice && slice.byteLength == sliceSize) {
+                var view = new Uint8Array(slice);
+                var arr = [];
+                view.forEach(function(v) {
+                    arr.push(v.toString(16));
+                });
+                view = null;
+                var idx = arr.join(' ').indexOf(imgType);
+                if (idx > -1) {
+                    back(typeName[idx]);
+                } else {
+                    back(false);
+                }
+            } else {
+                back(false);
+            }
+
+        });
+    } else {
+        back(file.type.match(/\/(\w+)/)[1]);
+    }
+}
